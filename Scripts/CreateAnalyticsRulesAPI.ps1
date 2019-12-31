e78703b01e96f8ac8af2f3c0d0e0bfe5628fd200
Javier Soriano <jasorian@microsoft.com>
2019-12-31 02:11:05 +0100

    variable change

--- Scripts/CreateAnalyticsRulesAPI.ps1
+++ Scripts/CreateAnalyticsRulesAPI.ps1
@@ -16,7 +16,7 @@ Import-Module AzSentinel
 $artifactName = "RulesFile"
 
 #Build the full path for the analytics rule file
-$artifactPath = Join-Path $env:Build_SourcesDirectory $artifactName 
+$artifactPath = Join-Path $env:Build_ArtifactStagingDirectory $artifactName 
 $rulesFilePath = Join-Path $artifactPath $RulesFile
 
 #Resource URL to authentincate against
