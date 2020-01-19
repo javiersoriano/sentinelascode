1c099fbdc26b7225ee35d1e6a32efa5ed89834ac
Javier Soriano <jasorian@microsoft.com>
2020-01-19 23:22:15 +0100

    onboarding changes

--- Scripts/InstallSentinel.ps1
+++ Scripts/InstallSentinel.ps1
@@ -1,18 +1,31 @@
 param (
     [Parameter(Mandatory=$true)]$ResourceGroup,
-    [Parameter(Mandatory=$true)]$Workspace
+    [Parameter(Mandatory=$true)]$Workspace,
+    [Parameter(Mandatory=$true)]$OnboardingFile
 )
 
 #Adding AzSentinel module
 Install-Module AzSentinel -Scope CurrentUser -Force
 Import-Module AzSentinel
 
-$solutions = Get-AzOperationalInsightsIntelligencePack -resourcegroupname $ResourceGroup -WorkspaceName $Workspace
+$artifactName = "OnboardingFile"
 
-if (($solutions | Where-Object Name -eq 'SecurityInsights').Enabled) {
-    Write-Error "SecurityInsights solution is already enabled for workspace $Workspace"
-    exit
+#Build the full path for the onboarding file
+$artifactPath = Join-Path $env:Pipeline_Workspace $artifactName 
+$onboardingFilePath = Join-Path $artifactPath $OnboardingFile
+
+$workspaces = Get-Content -Raw -Path $onboardingFilePath | ConvertFrom-Json
+
+foreach ($wrkspce in $workspaces.workspace){
+    Write-Host "Processing workspace $wrkspce ..."
+    $solutions = Get-AzOperationalInsightsIntelligencePack -resourcegroupname $ResourceGroup -WorkspaceName $wrkspce
+
+    if (($solutions | Where-Object Name -eq 'SecurityInsights').Enabled) {
+        Write-Error "SecurityInsights solution is already enabled for workspace $Workspace"
+        exit
+    }
+    else {
+        Set-AzSentinel -WorkspaceName $Workspace -Confirm:$false
+    }
 }
-else {
-    Set-AzSentinel -WorkspaceName $Workspace -Confirm:$false
-}
+
